378e84f1cac094a0426c493ac55bd7fb3ca0745b
akshayraut3105 <akshayraut3105@gmail.com>
2021-04-07 10:55:36 +0530

    [AKSHAY] added method to read write in JSON files

--- AddressBookDemo/Contact.cs
+++ AddressBookDemo/Contact.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace AddressBook
 {
-    class Contact
+    public class Contact
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
@@ -14,17 +14,6 @@ namespace AddressBook
         public string Email { get; set; }
         public int Zip { get; set; }
         public long PhoneNumber { get; set; }
-        public Contact(string firstName, string lastName, string address, string city, string state, string email, int zip, long phoneNumber)
-        {
-            FirstName = firstName;
-            LastName = lastName;
-            Address = address;
-            City = city;
-            State = state;
-            Email = email;
-            Zip = zip;
-            PhoneNumber = phoneNumber;
-        }
         public override bool Equals(object obj)
         {
             Contact contact = (Contact)obj;
---
+++ AddressBookDemo/JSONOperation.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace AddressBook
+{
+    public class JSONOperation
+    {
+        string filePath = @"C:\Users\ITeS\Desktop\NEW ADDRESSBOOK\New-Address-Book\AddressBookDemo\Utility\ABRecord.json";
+        public void WriteToFile(Dictionary<string, AddressBook> addressBookDictionary)
+        {
+            string json = "";
+            foreach (AddressBook obj in addressBookDictionary.Values)
+            {
+                foreach (Contact contact in obj.addressBook.Values)
+                {
+                    json = JsonConvert.SerializeObject(contact);
+                    File.WriteAllText(filePath, json);
+                }
+            }
+            Console.WriteLine("\nSuccessfully added to JSON file.");
+        }
+        public void ReadFromFile()
+        {
+            Console.WriteLine("Below are Contents of JSON File");
+            var json = File.ReadAllText(filePath);
+            Contact contact = JsonConvert.DeserializeObject<Contact>(json);
+            Console.WriteLine(contact.ToString());
+        }
+    }
+}
